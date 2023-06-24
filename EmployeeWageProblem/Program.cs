a75628140f09f366da1d3c577b5130356872ae28
MansiRavindraMohan <manasimohan29@gmail.com>
2023-06-24 23:39:11 +0530

    [Mansi]: Check Employee is Present or Absent

---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CheckAttendance checkAttendance = new CheckAttendance();
+            checkAttendance.CheckEmployee();
+            Console.ReadLine(); 
+        }
+    }
+}
