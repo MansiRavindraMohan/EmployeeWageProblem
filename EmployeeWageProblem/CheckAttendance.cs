4b5cbaa25ee0df1e5b71a7b8b2a141697ffcde77
MansiRavindraMohan <manasimohan29@gmail.com>
2023-06-25 00:03:55 +0530

    [Mansi]: Calculate daily employee wage and add part time employee wage

--- EmployeeWageProblem/CheckAttendance.cs
+++ EmployeeWageProblem/CheckAttendance.cs
@@ -12,19 +12,22 @@ namespace EmployeeWageProblem
         {
             int IS_FULL_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
+            int IS_PART_TIME = 2;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
 
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
                 empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
