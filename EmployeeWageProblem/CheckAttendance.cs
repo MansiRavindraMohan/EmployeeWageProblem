791517a433f7b633f58b7374c3db95dfbf918641
MansiRavindraMohan <manasimohan29@gmail.com>
2023-06-28 11:42:48 +0530

    [Mansi]: Calculate employee wages for a month till condition of total working hours

--- EmployeeWageProblem/CheckAttendance.cs
+++ EmployeeWageProblem/CheckAttendance.cs
@@ -12,13 +12,17 @@ namespace EmployeeWageProblem
         public const int EMP_RATE_PER_HOUR = 20;
         public const int IS_PART_TIME = 2;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 10;
         public void CheckEmployee()
         {
             int empHrs = 0;
             int empWage = 0;
             int totalEmpWage = 0;
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(3);
 
@@ -34,11 +38,10 @@ namespace EmployeeWageProblem
                         empHrs = 0;
                         break;
                 }
-
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp Wage: " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day # " + totalWorkingDays + " Emp Hrs " + empHrs);
             }
+            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage: " + totalEmpWage);
         }
     }
